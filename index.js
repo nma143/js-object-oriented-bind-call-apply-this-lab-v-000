7837175b458dee6e9be73505dc9e67d1cd1fba0f
Nikki A <nikki.apostolakis@gmail.com>
2019-04-23 13:53:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,8 @@ function setThisWithCall(fn, thisValue, arg)
 {
   return fn.call(thisValue, arg);
 }
+
+function setThisWithApply(fn, thisValue, args)
+{
+  return fn.apply(fn, thisValue, [args]);
+}
